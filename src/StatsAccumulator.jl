55a9589007058d1b46395575bf31b93ca978b62e
Martin Hinsch <hinsch.martin@gmail.com>
2024-12-04 09:33:14 +0000

    misc docs

--- src/StatsAccumulator.jl
+++ src/StatsAccumulator.jl
@@ -128,13 +128,11 @@ Construct a histogram accumulator.
 
 Arguments:
 
-* `min` - minimum value [T(0)]
-* `width` - bin size [T(1)]
-* `max` - maximum value. Set lower than or equal to `min` to let the histogram adjust size automatically. [min]
-
-Named arguments:
-* `count_below_min` - whether values lower than `min` are ignored or counted in the first bin
-* `count_above_max` - whether values larger than `max` are ignored or counted in the last bin
+* `min` - minimum value [`T(0)`]
+* `width` - bin size [`T(1)`]
+* `max` - maximum value. Set lower than or equal to `min` to let the histogram adjust size automatically. [`min`]
+* `count_below_min` - whether values lower than `min` are ignored or counted in the first bin [`false`]
+* `count_above_max` - whether values larger than `max` are ignored or counted in the last bin [`false`]
 """
 HistAcc(min::T = T(0), width::T = T(1), max::T = min; 
         count_below_min = false, count_above_max = false) where {T} = 
